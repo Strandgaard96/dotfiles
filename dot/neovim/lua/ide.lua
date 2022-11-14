ebe5ba93b51f3bb0ba1b80f3a519ccedbe4196af
Strandgaard96 <magnus.str@gmail.com>
2022-11-14 15:24:31 +0100

    Removed some ls functionality as it is giving me trouble.

--- dot/neovim/lua/ide.lua
+++ dot/neovim/lua/ide.lua
@@ -9,8 +9,6 @@ require("nvim-lsp-installer").setup({
     }
 })
 -- More like IDE
-require('lspconfig').pyright.setup{}
-require('lspconfig').bashls.setup{}
 
 require('nvim-autopairs').setup{}
 require("which-key").setup{}
@@ -26,8 +24,6 @@ require('lsp_signature').setup({
 })
 
 -- locals
-local lsp = vim.lsp
-local handlers = lsp.handlers
 local cmp = require'cmp'
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
@@ -122,61 +118,3 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 -- THIS GIES TROUBLE OF FEND SO I REMOVED IT
 --- Treesitter
-require('nvim-treesitter.install').compilers = { "clang", "gcc" }
-require'nvim-treesitter.configs'.setup {
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-    -- ignore_install = { "javascript" }, -- List of parsers to ignore installing
-    -- ensure_installed can be "all" or a list of languages { "python", "javascript" }
-    -- ensure_installed = "maintained",
-    ensure_installed = {
-        "bash",
-        "css",
-        "html",
-        "javascript",
-        "python",
-        "tsx",
-        "typescript",
-        "vue",
-        "svelte",
-    },
-
-    highlight = { -- enable highlighting for all file types
-      -- enable = true,
-    },
-
-    -- need for proper indentation handling (especially for bracket pairs)
-    indent = {
-      enable = true
-    },
-
-    incremental_selection = {
-      enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
-      disable = { "cpp", "lua" },
-      keymaps = {                       -- mappings for incremental selection (visual mappings)
-        init_selection = "gnn",         -- maps in normal mode to init the node/scope selection
-        node_incremental = "grn",       -- increment to the upper named parent
-        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
-        node_decremental = "grm",       -- decrement to the previous node
-      }
-    },
-
-    textobjects = {
-      -- These are provided by
-      select = {
-        enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
-        keymaps = {
-          -- You can use the capture groups defined here:
-          -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/c/textobjects.scm
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ab"] = "@block.outer",
-          ["ib"] = "@block.inner",
-          ["as"] = "@statement.outer",
-          ["is"] = "@statement.inner",
-        },
-      },
-    },
-}
