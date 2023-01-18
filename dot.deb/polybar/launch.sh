9ce6f35aa21c37355e15db3224c054e9caeeea7e
Strandgaard96 <magnus.str@gmail.com>
2023-01-18 18:02:02 +0100

    Working polybar example

--- dot.deb/polybar/launch.sh
+++ dot.deb/polybar/launch.sh
@@ -1,13 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar --config=~/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar -c /home/magstr/.config/polybar/config.ini mybar 2> /tmp/polybar_newlog1.log &
+
+echo "Polybar launched..."
