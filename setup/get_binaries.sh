85c526d8c1dc47c0aab902b58640ef8eaddb813f
Strandgaard96 <magnus.str@gmail.com>
2023-02-26 15:00:55 +0100

    Added get binaries examples for ripgrep.

---
+++ setup/get_binaries.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DWNLLDIR="$HOME/opt/download"
+mkdir -p $DWNLLDIR
+
+INSTALL_DIR="$HOME/opt"
+mkdir -p $INSTALL_DIR
+
+curl -L -o $DWNLLDIR/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
+tar xzf $DWNLLDIR/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz --directory $INSTALL_DIR
+
+mv $INSTALL_DIR/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg ~/bin
