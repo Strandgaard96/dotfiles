5293232efb578bb5d3c517db95e72b0dc4cce97f
Strandgaard96 <magnus.str@gmail.com>
2024-06-16 10:19:36 +0200

    adde fzf binds

--- dot/nvim/lua/plugins/lsp.lua
+++ dot/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,9 @@
 -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
+-- For LSP window border
+require("lspconfig.ui.windows").default_options.border = "single"
+
 return {
   -- lspconfig
   {
