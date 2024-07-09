e5a646b8e5f62c7406280c6fee363bcb5fea6d32
Strandgaard96 <magnus.str@gmail.com>
2024-07-09 20:49:10 +0200

    Working polybar and i3 config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
+
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c /home/magstr/.config/polybar/config.ini mybar 2> /tmp/polybar_newlog1.log &
+polybar -c /home/magstr/.config/polybar/config.ini mybar 2>/tmp/polybar_newlog1.log &
 
 echo "Polybar launched..."
+
