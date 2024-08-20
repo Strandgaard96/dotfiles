a9a83e04fd5a3bd9b0b42dd2d48a7d97541fc8a5
Strandgaard96 <magnus.str@gmail.com>
2024-08-20 12:40:36 +0200

    nice i3 setup

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,14 +8,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
+    MONITOR=$m polybar --reload mybar -c /home/magstr/.config/polybar/config.ini 2>/tmp/polybar_newlog1.log &
   done
 else
-  polybar --reload mybar &
+  polybar --reload mybar -c /home/magstr/.config/polybar/config.ini 2>/tmp/polybar_newlog1.log &
 fi
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c /home/magstr/.config/polybar/config.ini mybar 2>/tmp/polybar_newlog1.log &
+# polybar -c /home/magstr/.config/polybar/config.ini mybar 2>/tmp/polybar_newlog1.log &
 
 echo "Polybar launched..."
-
