a19d5f0011da07086fe038ef9ee68b2b885b6dd1
Strandgaard96 <magnus.str@gmail.com>
2024-08-20 12:41:09 +0200

    Working lazyvim backup file

--- nvim/.config/nvim/lua/plugins/extras/personal/oil.lua
+++ nvim/.config/nvim/lua/plugins/extras/personal/oil.lua
@@ -1,3 +1,9 @@
 return {
-  "stevearc/oil.nvim",
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "echasnovski/mini.icons" },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
 }
