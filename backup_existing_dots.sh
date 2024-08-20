a65f60caf57edc77fcf9497690f5030962723b6e
Strandgaard96 <magnus.str@gmail.com>
2024-08-20 12:41:09 +0200

    Working lazyvim backup file

---
+++ backup_existing_dots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the path to the .bashrc file and its backup
+BASHRC="$HOME/.bashrc"
+BACKUP="$HOME/.bashrc.bk"
+
+# Check if the .bashrc file exists
+if [ -f "$BASHRC" ]; then
+  # Move the .bashrc file to .bashrc.bk
+  mv "$BASHRC" "$BACKUP"
+  echo "Moved $BASHRC to $BACKUP"
+else
+  echo "$BASHRC does not exist"
+fi
