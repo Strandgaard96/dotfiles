c8494e5ba034690d2aa07485fd1b6735d96d410f
Strandgaard96 <magnus.str@gmail.com>
2025-05-18 21:46:42 +0200

    Add supermaven

---
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,31 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = {
+    {
+      "supermaven-inc/supermaven-nvim",
+      opts = {
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+      },
+    },
+    {
+      "huijiro/blink-cmp-supermaven",
+    },
+  },
+  opts = {
+    sources = {
+      default = { "lsp", "path", "supermaven", "snippets", "buffer" },
+      providers = {
+        supermaven = {
+          name = "supermaven",
+          module = "blink-cmp-supermaven",
+          async = true,
+        },
+      },
+    },
+    keymap = {
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+    },
+  },
+}
